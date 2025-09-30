38d8cf48b84c35940e3c0f977265f0d5ed569fa5
Dominik <93959@roc-teraa.nl>
2025-09-30 11:41:01 +0200

    Js HUH

--- scripts/fetchApi.js
+++ scripts/fetchApi.js
@@ -20,17 +20,6 @@ async function fetchMeraki() {
     }
 }
 
-async function fetchPlesk() {
-    try {
-        const res = await fetch("api/pleskdata.php");
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        const data = await res.json();
-        console.log("Plesk:", data);
-    } catch (err) {
-        console.error("Failed to fetch Plesk:", err);
-    }
-}
-
 // run immediately + every 60s
 fetchUnifi();
 fetchMeraki();
--- scripts/script.js
+++ scripts/script.js
@@ -3,103 +3,97 @@ let serverData = [];
 const title = document.querySelector("title");
 
 function init() {
-  renderCards()
-  renderProgressBars()
-  setTimeout(() => {
-    init()
-  }, 600000);
+    renderCards(allData);
+    renderProgressBars(serverData);
+    setTimeout(() => {
+        init();
+    }, 60000);
 }
 
 //fetch("api/pleskdata.php?file=plesk_status")
 fetch("https://pleskdata.qnimbus.nl/plesk_status.json")
-  .then(response => response.json())
-  .then(data => {
-    allData = data; 
-    renderCards(allData);
-  })
-  .catch(error => console.error("Error loading JSON:", error));
+    .then((response) => response.json())
+    .then((data) => {
+        allData = data;
+        renderCards(allData);
+    })
+    .catch((error) => console.error("Error loading JSON:", error));
 
-  //fetch("api/pleskdata.php?file=plesk_stats")
+//fetch("api/pleskdata.php?file=plesk_stats")
 fetch("https://pleskdata.qnimbus.nl/plesk_stats.json")
-  .then(response => response.json())
-  .then(data => {
-    serverData = data.server.get.result.stat;
-      // console.log(serverData);
-      renderProgressBars(serverData);
-  })
-  .catch(error => console.error("Error loading JSON:", error));
+    .then((response) => response.json())
+    .then((data) => {
+        serverData = data.server.get.result.stat;
+        // console.log(serverData);
+        renderProgressBars(serverData);
+    })
+    .catch((error) => console.error("Error loading JSON:", error));
 
 function renderCards(data) {
-  const container = document.querySelector("#card-container");
-  container.innerHTML = ""; 
-  // console.log(data);
-  data.forEach(item => {
-    const card = document.createElement("a");
-    card.href= "https://" + item.url;
-    card.target = "_blank";
-    card.classList.add("card");
+    const container = document.querySelector("#card-container");
+    container.innerHTML = "";
+    // console.log(data);
+    data.forEach((item) => {
+        const card = document.createElement("a");
+        card.href = "https://" + item.url;
+        card.target = "_blank";
+        card.classList.add("card");
 
-    const status = Number(item.status);
-	
-	if (status < 100) {
-	  card.classList.add("status-0xx");
-	}
-    else if (status >= 200 && status <= 299) {
-      card.classList.add("status-2xx");
-    } 
-    else if (status >= 300 && status <= 399) {
-      card.classList.add("status-3xx");
-    } 
-    else if (status >= 400 && status <= 499) {
-      card.classList.add("status-4xx");
-      title.innerHTML = `404 ${item.url} DOWN`
-    } 
-    else if (status >= 500 && status <= 599) {
-      card.classList.add("status-5xx");
-    } 
-    else if (status >= 100 && status <= 199) {
-      card.classList.add("status-1xx");
-    }
+        const status = Number(item.status);
 
-    card.innerHTML = `
+        if (status < 100) {
+            card.classList.add("status-0xx");
+        } else if (status >= 200 && status <= 299) {
+            card.classList.add("status-2xx");
+        } else if (status >= 300 && status <= 399) {
+            card.classList.add("status-3xx");
+        } else if (status >= 400 && status <= 499) {
+            card.classList.add("status-4xx");
+            title.innerHTML = `404 ${item.url} DOWN`;
+        } else if (status >= 500 && status <= 599) {
+            card.classList.add("status-5xx");
+        } else if (status >= 100 && status <= 199) {
+            card.classList.add("status-1xx");
+        }
+
+        card.innerHTML = `
       <p class="url">${item.url}</p>
     `;
 
-    container.appendChild(card);
+        container.appendChild(card);
 
-    const urlBox = card.querySelector(".url");
-  });
+        const urlBox = card.querySelector(".url");
+    });
 }
 
 function renderProgressBars(serverData) {
-  fetchPlesk()
-  const memTotal = parseInt(serverData.mem.total, 10);
-  const memUsed  = parseInt(serverData.mem.used, 10);
-  const memPercent = (memUsed / memTotal) * 100;
+    const memTotal = parseInt(serverData.mem.total, 10);
+    const memUsed = parseInt(serverData.mem.used, 10);
+    const memPercent = (memUsed / memTotal) * 100;
 
-  if (memPercent >= 80 && memPercent < 90) {
-    memClass = "orange";
-  } else if (memPercent >= 90) {
-    memClass = "red";
-  } else {
-    memClass = "green";
-  }
+    if (memPercent >= 80 && memPercent < 90) {
+        memClass = "orange";
+    } else if (memPercent >= 90) {
+        memClass = "red";
+    } else {
+        memClass = "green";
+    }
 
-  const diskTotal = parseInt(serverData.diskspace.device.total, 10);
-  const diskUsed  = parseInt(serverData.diskspace.device.used, 10);
-  const diskPercent = (diskUsed / diskTotal) * 100;
+    const diskTotal = parseInt(serverData.diskspace.device.total, 10);
+    const diskUsed = parseInt(serverData.diskspace.device.used, 10);
+    const diskPercent = (diskUsed / diskTotal) * 100;
 
-  if (diskPercent >= 80 && diskPercent < 90) {
-    diskClass = "orange";
-  } else if (diskPercent >= 90) {
-    diskClass = "red";
-  } else {
-    diskClass = "green";
-  }
+    if (diskPercent >= 80 && diskPercent < 90) {
+        diskClass = "orange";
+    } else if (diskPercent >= 90) {
+        diskClass = "red";
+    } else {
+        diskClass = "green";
+    }
 
-  const barUp = document.querySelector(".footUp");
-  barUp.innerHTML = ""
-  barUp.innerHTML = `
+    const barUp = document.querySelector(".footUp");
+    barUp.innerHTML = "";
+    barUp.innerHTML = `
   <div class="memoryBar ${memClass}">
     <p>Memory used:</p>
     <progress max="${memTotal}" value="${memUsed}"></progress>
@@ -110,42 +104,40 @@ function renderProgressBars(serverData) {
     <progress max="${diskTotal}" value="${diskUsed}"></progress>
     <p>${diskPercent.toFixed(2)}%</p>
   </div>
-  `
+  `;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const filter = document.getElementById("status-filter");
+    const filter = document.getElementById("status-filter");
 
-filter.addEventListener("change", () => {
-  const value = filter.value;
-  if (value === "all") {
-    renderCards(allData);
-  } 
-  else {
-    const [min, max] = value.split("-").map(Number);
-    const filtered = allData.filter(item => {
-      const status = Number(item.status);
-      return status >= min && status <= max;
+    filter.addEventListener("change", () => {
+        const value = filter.value;
+        if (value === "all") {
+            renderCards(allData);
+        } else {
+            const [min, max] = value.split("-").map(Number);
+            const filtered = allData.filter((item) => {
+                const status = Number(item.status);
+                return status >= min && status <= max;
+            });
+            renderCards(filtered);
+        }
     });
-    renderCards(filtered);
-  }
-});
 
-const themeSelect = document.getElementById("theme-select");
+    const themeSelect = document.getElementById("theme-select");
 
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark-mode");
-  themeSelect.value = "dark";
-}
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+        themeSelect.value = "dark";
+    }
 
-themeSelect.addEventListener("change", (e) => {
-  if (e.target.value === "dark") {
-    document.body.classList.add("dark-mode");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.body.classList.remove("dark-mode");
-    localStorage.setItem("theme", "light");
-  }
-  
-});
+    themeSelect.addEventListener("change", (e) => {
+        if (e.target.value === "dark") {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("theme", "light");
+        }
+    });
 });
